0ecc76453d56a921cb23dc83d6682bbbf4aa2611
joychoSMHC <tyo@smhc.co.jp>
2019-02-14 14:27:30 +0900

    create auto-test-sample-test-suite

---
+++ test-Sample-for-AppointSystem/Test Suites/testSample.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testSample</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ed4acbd6-7a48-49e5-a7b5-e3492ef20c4a</testSuiteGuid>
+   <testCaseLink>
+      <guid>698c251b-1b08-4c9e-b614-03d84f403681</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Firtst Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
